c02a12e3c2e5eb032d74c3601415569cf5f1fc89
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2022-11-16 12:54:46 +0700

    Update: comment

--- SkyFight/src/input/MouseState.java
+++ SkyFight/src/input/MouseState.java
@@ -4,7 +4,5 @@ public enum MouseState {
 	RIGHTPRESSED,
 	LEFTPRESSED,
 	RELEASED,
-	ENTERED,
-	EXITED,
 	NONE
 }
--- SkyFight/src/input/MyMouseListener.java
+++ SkyFight/src/input/MyMouseListener.java
@@ -6,12 +6,11 @@ import java.awt.event.MouseListener;
 
 import frame.Screen;
 
-
 public class MyMouseListener implements MouseListener{
 	
-	private Screen myScreen;
-	
 	boolean oneClicked = false;
+	
+	private Screen myScreen;
 	
 	public MyMouseListener(Screen myScreen) {
 		this.myScreen = myScreen;
@@ -23,44 +22,36 @@ public class MyMouseListener implements MouseListener{
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if(!oneClicked) {
-			//1. get position pressed mouse
-			// pointerInfor - panel
-			int xPressedMouse = MouseInfo.getPointerInfo().getLocation().x - myScreen.getContentPane().getLocationOnScreen().x;
-			int yPressedMouse = MouseInfo.getPointerInfo().getLocation().y - myScreen.getContentPane().getLocationOnScreen().y;
-			
-			myScreen.setMousePosition(xPressedMouse, yPressedMouse);
-			if(e.getButton() == MouseEvent.BUTTON1)
-				myScreen.setIsPressedMouse(MouseState.LEFTPRESSED);
-			else if(e.getButton() == MouseEvent.BUTTON3)
-				myScreen.setIsPressedMouse(MouseState.RIGHTPRESSED);
-
-			//2. set position to gameControl
-			//debug
-			// print position pressed mouse
-//			System.out.println(xPressedMouse + " " + yPressedMouse);	
-			oneClicked = true;
-		}
+		//1. get position pressed mouse
+		// pointerInfor - panel
+		int xPressedMouse = MouseInfo.getPointerInfo().getLocation().x - myScreen.getContentPane().getLocationOnScreen().x;
+		int yPressedMouse = MouseInfo.getPointerInfo().getLocation().y - myScreen.getContentPane().getLocationOnScreen().y;
+		//2. set position to gameControl
+		myScreen.setMousePosition(xPressedMouse, yPressedMouse);
+		if(e.getButton() == MouseEvent.BUTTON1)
+			myScreen.setIsPressedMouse(MouseState.LEFTPRESSED);
+		else if(e.getButton() == MouseEvent.BUTTON3)
+			myScreen.setIsPressedMouse(MouseState.RIGHTPRESSED);
+		
+		//debug
+		// print position pressed mouse
+//		System.out.println(xPressedMouse + " " + yPressedMouse);	
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if(oneClicked) {	
-			myScreen.setMousePosition(-1, -1);
-			myScreen.setIsPressedMouse(MouseState.RELEASED);
-			oneClicked = false;
-		}
+		//reset mouse position and state
+		myScreen.setMousePosition(-1, -1);
+		myScreen.setIsPressedMouse(MouseState.RELEASED);
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-//		myScreen.setIsPressedMouse(MouseState.RELEASED);
 
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-//		myScreen.setIsPressedMouse(MouseState.RELEASED);
 
 	}
 
